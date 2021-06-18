21e8b0f9497886c5062421e772f03b0aabe88676
Manie-oss <to.mansip@gmail.com>
2021-06-18 14:31:14 +0530

    sent request to API server using axios library

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-
-  submitForm(){
-    
+  constructor(props){
+    super(props)
+      this.state = {
+        name : "",
+        password : ""
+    }
   }
+
+  submitForm = (event) =>  {
+    console.log(this.state);
+    axios.post('http://localhost:8080/login', this.state)
+    .then(response => {
+      console.log(response)
+    }) 
+    .catch(error => {
+      console.log(error)
+    })
+  }
+    
 
   render() {
     return(
       <div className="App">
         <form>
-          <input type="text" name="name" placeholder = "UserName"/><br />
-          <input type="password" name="pass" placeholder = "Password"/><br />
+          <input type="text" id = "uname" name="name" placeholder = "UserName" onChange = {(event) => this.setState({ ...this.setState, name: event.target.value})}/><br />
+          <input type="password" id = "pass" name="pass" placeholder = "Password" onChange = {(event) => this.setState({ ...this.setState, password: event.target.value})}/><br />
         </form>
-        <button onClick={ () => this.submitForm }>LogIn</button>
+        <button type = "button" onClick = { this.submitForm }>LogIn</button>
     </div>
     );
   }
